fcb5dff0c719aae269bc6b347c97e1bcba271127
faha1999 <kafahad1999@gmail.com>
2021-12-05 20:01:56 +0600

    applied objects disfunction and poperty existance

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -20,10 +20,14 @@ const renderMovies = (filter = '') => {
 
   filteredMovie.forEach((movie) => {
     const movieEl = document.createElement('li');
-    let text = movie.info.title + ' - ';
-    for (const key in movie.info) {
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
+    // const { title: movieTitle } = info;
+    // const { getFormattedTitle } = movie;
+    let text = movie.getFormattedTitle() + ' - ';
+    for (const key in info) {
       if (key !== 'title') {
-        text = text + `${key}: ${movie.info[key]}`;
+        text = text + `${key}: ${info[key]}`;
       }
     }
     movieEl.textContent = text;
@@ -49,7 +53,10 @@ const addMovieHandler = () => {
       title,
       [extraName]: extraValue
     },
-    id: Math.random()
+    id: Math.random().toString(),
+    getFormattedTitle: function () {
+      return this.info.title.toUppercase();
+    }
   };
 
   movies.push(newMovie);
